6117d358d75a5cb5412fc7eba03676c4cdf2419c
user <usero@tronix.localhost>
2019-11-12 16:21:40 +0100

    Update config

--- src/Settings/Config.php
+++ src/Settings/Config.php
@@ -8,15 +8,15 @@ class Config
 	const MYSQL_USER = 'root';
 	const MYSQL_PASS = 'toor';
 	const MYSQL_PORT = 3306;
-	const MYSQL_DBNAME = 'tronix';
+	const MYSQL_DBNAME = 'dbname';
 
 	// Smtp settings
 	const SMTP_USER = '';
 	const SMTP_PASS = '';
 	const SMTP_HOST = '127.0.0.1';
 	const SMTP_PORT = 25; // 25, 587, 465
-	const SMTP_FROM_EMAIL = 'biuro@domain.here';
-	const SMTP_FROM_USER = 'Biuro';
+	const SMTP_FROM_EMAIL = 'email@domain.here';
+	const SMTP_FROM_USER = 'User Name';
 	const SMTP_TLS = false;
 	const SMTP_AUTH = false;
 	const SMTP_DEBUG = 0;
