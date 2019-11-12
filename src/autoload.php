c6acf100d1493d7bc999d99794875357387c0d42
user <usero@tronix.localhost>
2019-11-12 18:28:52 +0100

    Custom autoload update

--- src/autoload.php
+++ src/autoload.php
@@ -1,10 +1,12 @@
 <?php
 // autoload php classes from namespace sample without composer
 spl_autoload_register(function($class) {
-	// convert namespace to full file path
-	$class = str_replace("Xtra\\","",$class); // remove Xtra\\ from path
-	// Load class file
-	$class = $_SERVER['DOCUMENT_ROOT'].'/src/' . str_replace('\\', '/', $class) . '.php';
+	// Load from folder
+	// $class = $_SERVER['DOCUMENT_ROOT'].'/src/' . str_replace('\\', '/', $class) . '.php';
+	// Load from current folder
+	$class = str_replace("Xtra","",$class); // remove Xtra
+	$class = ltrim($class,'\');
+	$class = str_replace('\\', '/', $class) . '.php';
 	// Load class if exists
 	if (file_exists($class)) {
 		if (!class_exists($class)) {
