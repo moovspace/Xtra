8474f8902c2bba2b198cb410ce40cb206bd36d11
user <usero@tronix.localhost>
2019-11-12 19:34:36 +0100

    Php show image secure

--- src/Image/ImageSecureShow.php
+++ src/Image/ImageSecureShow.php
@@ -1,27 +1,51 @@
 <?php
 namespace Xtra\Image;
+use \Exception;
 
-/**
- *  Show iamge
- */
 class ImageSecureShow
 {
-    function __construct(){}
+	public $MimeTypes = ['gif'=> 'image/gif', 'png'=> 'image/png', 'jpeg'=> 'image/jpeg', 'jpg'=> 'image/jpeg'];
 
-    function ShowImage($name){
-        $file = basename($name);
-        $path  = 'img/'.$file;
-        $mime = mime_content_type($path);
-        $types = [ 'gif'=> 'image/gif', 'png'=> 'image/png', 'jpeg'=> 'image/jpeg', 'jpg'=> 'image/jpeg'];
-        // if allowed type
-        if(in_array($mime, $types)){
-            if(file_exists($path)){
-                header('Content-type: '.$mime);
-                header("Content-Length: " . filesize($path));
-                //echo file_get_contents($path);
-                readfile($path);
-            }
-        }
-    }
+	function __construct(){}
+
+	function ShowImage($path){
+		if(file_exists($path)){
+			$mime = mime_content_type($path);
+			// if allowed type
+			if(in_array($mime, $this->MimeTypes)){
+				header('Content-type: '.$mime);
+				header("Content-Length: " . filesize($path));
+				readfile($path);
+				//echo file_get_contents($path);
+			}else{
+				throw new Exception("Error file extension", 1);
+			}
+		}else{
+			throw new Exception("Error file path", 1);
+		}
+	}
 }
 ?>
+
+/*
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+try{
+	// Test if correct key
+	if($_SESSION['user']['key'] == 'secret'){
+		// Create object
+		$img = new ImageSecureShow();
+		// Change types
+		$img->MimeTypes = ['png'=> 'image/png', 'jpeg'=> 'image/jpeg', 'jpg'=> 'image/jpeg'];
+		// Show image
+		$img->ShowImage('/media/image.jpg');
+	}
+
+}catch(Exception $e){
+	print_r($e);
+}
+?>
+*/
